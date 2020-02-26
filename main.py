c580e74a252019162eda5362909899a63a66efc3
sokolovados <sokolovadosolo@gmail.com>
2020-02-26 09:30:48 +0300

    commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dictGen import allvlangen
 from dictGen import untagged
 from snmpVLAN import snmp
 from pprint import pprint
-
+from jinja2 import Template,Environment, FileSystemLoader
 
 snmpout1 = snmp('10.220.43.168','nfrnjrfr','.1.3.6.1.2.1.17.7.1.4.3.1.1') #community и ip  в переменных
 
@@ -11,10 +11,16 @@ listofvlan = allvlangen(snmpout1) #все вланы на коммуте
 snmpout2 = snmp('10.220.43.168','nfrnjrfr','.1.3.6.1.2.1.17.7.1.4.3.1.4')
 snmpout3 = snmp('10.220.43.168','nfrnjrfr','.1.3.6.1.2.1.17.7.1.4.3.1.2')
 
-dict = untagged(snmpout2,snmpout3)
-#for k,v in untaggeddict.items():
-#    print(k + ':' +v)
-pprint(dict)
+vlanport = untagged(snmpout2,snmpout3)
+pprint(vlanport)
+
+env = Environment(loader=FileSystemLoader('/home/python_projects/snmppars'))
+template = env.get_template('template.txt')
+with open('result.txt','w') as f:
+    f.write(template.render(vlanport))
+
+
+
 
 
 
